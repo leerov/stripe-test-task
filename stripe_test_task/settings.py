1759f132b655f82b2759b2fa98514ee804296265
harveyfa <ZhmulevVV@gmail.com>
2026-07-31 20:02:41 +0700

    fix: ensure STATIC_ROOT is explicitly set as string and remove duplicate STATIC_URL

--- stripe_test_task/settings.py
+++ stripe_test_task/settings.py
@@ -61,7 +61,8 @@ DATABASES = {
     )
 }
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+STATIC_ROOT = str(BASE_DIR / 'staticfiles')
 
 STORAGES = {
     "default": {
@@ -83,8 +84,6 @@ LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
-
-STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
